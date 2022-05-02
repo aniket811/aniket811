b0a9dafdcd3081c8161422a96a1cac3d4d81ce7f
Aniket Joshi <ajosh4347@gmail.com>
2022-05-02 22:19:23 +0530

    update in maps

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -1,6 +1,6 @@
 function initMap() {
     // Latitude and Longitude
-    var myLatLng = {lat: -6.207690, lng: 106.985270};
+    var myLatLng = {lat: 21.020034, lng: 75.542638};
 
     var map = new google.maps.Map(document.getElementById('google-maps'), {
         zoom: 17,
@@ -10,6 +10,6 @@ function initMap() {
     var marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
-        title: 'South Jakarta, INA' // Title Location
+        title: 'Jalgaon,Maharashta,India' // Title Location
     });
 }
